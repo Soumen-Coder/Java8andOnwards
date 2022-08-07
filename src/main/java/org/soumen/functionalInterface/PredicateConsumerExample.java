38d7137626177fb662d151eac299f8fe48b0f409
s0umen <soumen.raj3@gmail.com>
2022-08-08 01:32:23 +0530

    Functional Interfaces - Predicate-Consumer-BiConsumer Example, with instance level declaration

---
+++ src/main/java/org/soumen/functionalInterface/PredicateConsumerExample.java
@@ -0,0 +1,30 @@
+package org.soumen.functionalInterface;
+
+import org.soumen.functionalInterface.data.Student;
+import org.soumen.functionalInterface.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class PredicateConsumerExample {
+    Predicate<Student> p1 = student -> student.getGpa()>=2;
+    Predicate<Student> p2 = student -> student.getGradeLevel()>=3;
+
+    BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println("Student Name :- "+name+ " and Student activities :-"+activities);
+    Consumer<Student> studentConsumer = (student -> {
+        if(p1.and(p2).test(student)){
+            studentBiConsumer.accept(student.getName(), student.getActivities());
+        }
+    });
+
+    public void printNameAndActivities(List<Student> studentList){
+        studentList.forEach(studentConsumer);
+    }
+
+    public static void main(String[] args) {
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        new PredicateConsumerExample().printNameAndActivities(studentList);
+    }
+}
