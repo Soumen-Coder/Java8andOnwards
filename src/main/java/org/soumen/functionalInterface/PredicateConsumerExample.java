df88deff8f3d58b4f0317b1d26aeb89117082aca
s0umen <soumen.raj3@gmail.com>
2022-08-10 15:44:54 +0530

    Functional Interfaces - Predicate-Consumer-BiConsumer-BiPredicate adding BiPredicate to the example

--- src/main/java/org/soumen/functionalInterface/PredicateConsumerExample.java
+++ src/main/java/org/soumen/functionalInterface/PredicateConsumerExample.java
@@ -5,6 +5,7 @@ import org.soumen.functionalInterface.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,9 +13,14 @@ public class PredicateConsumerExample {
     Predicate<Student> p1 = student -> student.getGpa()>=2;
     Predicate<Student> p2 = student -> student.getGradeLevel()>=3;
 
+    BiPredicate<Integer, Double> biPredicate = (gradeLevel, gpa) -> gradeLevel>=3 && gpa>=3.5;
+
     BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println("Student Name :- "+name+ " and Student activities :-"+activities);
     Consumer<Student> studentConsumer = (student -> {
         if(p1.and(p2).test(student)){
+            studentBiConsumer.accept(student.getName(), student.getActivities());
+        }
+        if(biPredicate.test(student.getGradeLevel(), student.getGpa())){
             studentBiConsumer.accept(student.getName(), student.getActivities());
         }
     });
