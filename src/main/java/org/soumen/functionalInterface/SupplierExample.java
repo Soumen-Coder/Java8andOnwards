40de1b4c8ebb14589d85e48b4bebb7c198126d1f
s0umen <soumen.raj3@gmail.com>
2022-08-13 02:59:35 +0530

    SupplierExample.java

---
+++ src/main/java/org/soumen/functionalInterface/SupplierExample.java
@@ -0,0 +1,21 @@
+package org.soumen.functionalInterface;
+
+import org.soumen.functionalInterface.data.Student;
+import org.soumen.functionalInterface.data.StudentDataBase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierExample {
+    static Supplier<Student> studentSupplier = () -> new Student("Adam",2,3.6, "male", Arrays.asList("swimming", "basketball","volleyball"));
+
+    static Supplier<List<Student>> studentListSupplier = StudentDataBase::getAllStudents;
+
+    public static void main(String[] args) {
+
+        System.out.println(studentSupplier.get());
+
+        System.out.println(studentListSupplier.get());
+    }
+}
